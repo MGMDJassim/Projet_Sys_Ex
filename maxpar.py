e62968afe1dae97148e2e155339a50ba6836eeba
Jassim Mougamadou <80199377+MGMDJassim@users.noreply.github.com>
2025-02-06 23:36:20 +0100

    Commit

--- maxpar.py
+++ maxpar.py
@@ -5,13 +5,9 @@ class Task:
         self.writes = writes if writes is not None else []
         self.run = run
 
-X = 0
-Y = 0
-Z = 0
-
 def runT1():
     global X
-    X = int(input("Entrez un nombre: "))
+    X = X
 
 def runT2():
     global Y
@@ -21,28 +17,3 @@ def runTsomme():
     global X, Y, Z
     Z = X + Y
 
-t1 = Task()
-t1.name = "T1"
-t1.writes = ["X"]
-t1.run = runT1
-
-t2 = Task()
-t2.name = "T2"
-t2.writes = ["Y"]
-t2.run = runT2
-
-tSomme = Task()
-tSomme.name = "somme"
-tSomme.reads = ["X", "Y"]
-tSomme.writes = ["Z"]
-tSomme.run = runTsomme
-
-
-def main():
-    t1.run()
-    t2.run()
-    tSomme.run()
-    print("Z = ", Z)
-
-if __name__ == "__main__":
-    main()
