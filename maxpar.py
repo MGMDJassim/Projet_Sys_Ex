350bba920a38f727d1caac1d2fe421b37cf389e8
Jassim Mougamadou <80199377+MGMDJassim@users.noreply.github.com>
2025-02-20 20:02:16 +0100

    ajout de la méthode draw

--- maxpar.py
+++ maxpar.py
@@ -9,9 +9,30 @@ class TaskSystem:
     def __init__(self, tasks, precedence):
         self.tasks = tasks
         self.precedence = precedence
+
+    def draw(self):
+        # dessine le graphe de dépendances
+        pass
         
+    def _valide_input(self):
+        # vérifier si la liste des tâches est valide
+        #détection des doublons
+        task_names = set(self.tasks.keys())
 
+        if len(task_names) != len(self.tasks):
+            raise ValueError("Doublons dans les noms de tâches")
+        
+        #détection des dépendances avec des tâches inexistantes
+        for t in self.tasks:
+            for d in self.tasks[t].reads + self.tasks[t].writes:
+                if d not in task_names:
+                    raise ValueError(f"Tâche {t} dépend de tâche inexistante {d}")
 
+        #détection à un systeme de tâche intermédiaire
+        for t in self.tasks:
+            for d in self.tasks[t].reads + self.tasks[t].writes:
+                if d in self.tasks:
+                    raise ValueError(f"Tâche {t} dépend de tâche intermédiaire {d}")
 
     def getDependencies(self, nomTache):
         # retourne la liste des tâches qui doivent être exécuter avant nomTache
